02af71181d642c8c37516a4fb8ceccf491a85af0
Christopher De Leon <cdeleon@forbes.com>
2017-10-21 09:57:51 -0400

    finished prototype of the modal component

--- app/components/projects/ProjectModalComponent.js
+++ app/components/projects/ProjectModalComponent.js
@@ -6,7 +6,9 @@ const ProjectModalComponent = props => {
     <div id="myModal" className={`modal${modalClasses}`}>
       <div className="modal-content">
         <div className="modal-header">
-          {props.project.title}
+          <div className="modal-header-title">
+            {props.project.title}
+          </div>
           <span
             className="close"
             onClick={props.onModalClose}
@@ -16,6 +18,8 @@ const ProjectModalComponent = props => {
         </div>
         <div className="modal-text">
           <p>{props.project.description}</p>
+          <p><strong>Technology Stack: </strong>{props.project.technology}</p>
+          <p><a href={props.project.link}>Check it out</a></p>
         </div>
       </div>
     </div>
